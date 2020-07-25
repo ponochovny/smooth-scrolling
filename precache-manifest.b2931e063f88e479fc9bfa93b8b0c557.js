4dec224adcde10db4518c6463a96c4ba485b8782
ponochovny <ponochovny@gmail.com>
2020-07-25 16:47:51 +0300

    Updates

---
+++ precache-manifest.b2931e063f88e479fc9bfa93b8b0c557.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "073e16b460754b1fe2301218c4791b51",
+    "url": "/smooth-scrolling/index.html"
+  },
+  {
+    "revision": "3a7a6a180b7ceb9a398b",
+    "url": "/smooth-scrolling/static/css/main.cb2677b3.chunk.css"
+  },
+  {
+    "revision": "10ebc5335ee27c649394",
+    "url": "/smooth-scrolling/static/js/2.73ed7a84.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/smooth-scrolling/static/js/2.73ed7a84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a7a6a180b7ceb9a398b",
+    "url": "/smooth-scrolling/static/js/main.f3c782b0.chunk.js"
+  },
+  {
+    "revision": "66fa7d418e696dab3959",
+    "url": "/smooth-scrolling/static/js/runtime-main.1a6b6f3d.js"
+  }
+]);
